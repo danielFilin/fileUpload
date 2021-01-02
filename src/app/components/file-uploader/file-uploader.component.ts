0708be0c479d1ce47360ecf137a01f52fe652e66
Daniel <filind85@gmail.com>
2021-01-02 13:43:54 +0200

    added upload bar

--- src/app/components/file-uploader/file-uploader.component.ts
+++ src/app/components/file-uploader/file-uploader.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireStorage } from '@angular/fire/storage';
 import { FormControl, FormGroup } from '@angular/forms';
-import { pipe } from 'rxjs';
+import { Observable, pipe } from 'rxjs';
 import { finalize} from 'rxjs/operators';
 
 @Component({
@@ -12,10 +12,9 @@ import { finalize} from 'rxjs/operators';
 export class FileUploaderComponent implements OnInit {
   message: string;
   selectedFiles: FileList;
+  progressInfos = [];
 
-  formTemplate = new FormGroup({
-    caption: new FormControl('')
-  })
+  fileInfos: Observable<any>;
 
   constructor(private storage: AngularFireStorage) { }
 
@@ -27,17 +26,62 @@ export class FileUploaderComponent implements OnInit {
     this.message = '';
 
     for (let i = 0; i < this.selectedFiles.length; i++) {
-       let filePath = `${this.selectedFiles[i]}_${new Date().getTime()}`;
-       const fileRef = this.storage.ref(filePath);
-       this.storage.upload(filePath, this.selectedFiles[i]).snapshotChanges()
-       .pipe(finalize(()=> {
-         fileRef.getDownloadURL().subscribe((url) => {
-           console.log(url);
-         })
-       })
-       ).subscribe();
+        this.upload(i, this.selectedFiles[i])
     }
   }
 
+  upload(idx, file) {
+    this.progressInfos[idx] = { value: 0, fileName: file.name };
+    console.log(this.progressInfos[idx]);
+
+    //let filePath = `${file}_${new Date().getTime()}`;
+    const filePath = file.name;
+    const fileRef = this.storage.ref(filePath);
+    const task = this.storage.upload(filePath, file);
+
+    task.percentageChanges()
+    .pipe(finalize(()=> {
+      fileRef.getDownloadURL().subscribe((url) => {
+        //this.progressInfos[idx].value = Math.round(100 * url.loaded / url.total);
+      })
+    })
+    )
+    .subscribe((res) => {
+      this.progressInfos[idx].value = res;
+      console.log(res);
+    }),
+      err => {
+        this.progressInfos[idx].value = 0;
+        this.message = 'Could not upload the file:' + file.name;
+      };
+  };
+
+  // stopUpload(fileName, file) {
+  //   const task = this.storage.upload(fileName, file);
+
+  // }
+
+
+  // upload(idx, file) {
+  //   this.progressInfos[idx] = { value: 0, fileName: file.name };
+
+  //   let filePath = `${file}_${new Date().getTime()}`;
+  //   const fileRef = this.storage.ref(filePath);
+  //   this.storage.upload(filePath, file).snapshotChanges( )
+  //   .pipe(finalize(()=> {
+  //     fileRef.getDownloadURL().subscribe((url) => {
+  //       this.progressInfos[idx].value = Math.round(100 * url.loaded / url.total);
+  //       console.log(url);
+  //     })
+  //   })
+  //   )
+  //   .subscribe((res) => {
+  //     console.log(res);
+  //   }),
+  //     err => {
+  //       this.progressInfos[idx].value = 0;
+  //       this.message = 'Could not upload the file:' + file.name;
+  //     };
+  // }
 
 }
--- src/app/components/fileUpload.service.ts
+++ src/app/components/fileUpload.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FileUploadService {
+  fileDetailList:AngularFireList<any>;
 
-  constructor() { }
+  constructor(private firebase: AngularFireDatabase) { }
+
+  getFileDetailList() {
+    this.fileDetailList = this.firebase.list('fileDetails');
+  }
+
+  insertImageDetails(fileDetails) {
+    this.fileDetailList.push(fileDetails);
+  }
 }
